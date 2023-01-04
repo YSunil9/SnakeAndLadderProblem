8bd10b76f73866c025cbbb22d143109241dd2792
Sunil Kumar <96322711+YSunil9@users.noreply.github.com>
2023-01-04 22:47:37 +0530

    Update Player.cs

--- SnakeAndLadderProblem/Player.cs
+++ SnakeAndLadderProblem/Player.cs
@@ -12,7 +12,7 @@ namespace SnakeAndLadderProblem
         {
             Console.WriteLine("Welcome To Snake And Ladder Problem");
             int position = 0, player1;
-            Console.WriteLine("Single player at start position",/n +position);           
+            Console.WriteLine("Single player at start position", +position);           
         }
             public static void RollDice()
             {
