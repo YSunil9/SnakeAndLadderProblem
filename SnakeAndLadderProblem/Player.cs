9c666448fe5355bbfa9a276ef94fd228250a103c
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-04 16:10:31 +0530

    Welcome to Snake & Ladder UseCases

---
+++ SnakeAndLadderProblem/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    internal class Player
+    {
+        public static void Pos1()
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Problem");
+            int position = 0, player1 ;
+            Console.WriteLine("Single player at start position", +position);
+        }
+        
+    }
+}
---
+++ SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Player.Pos1();
+        }
+    }
+}
