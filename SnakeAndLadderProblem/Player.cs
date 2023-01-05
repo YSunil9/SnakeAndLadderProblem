80485677e695fc8204f698eaba83fe86f79f9a44
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-05 14:09:14 +0530

    [SUNIL]:Reaches win pos

--- SnakeAndLadderProblem/Player.cs
+++ SnakeAndLadderProblem/Player.cs
@@ -8,38 +8,46 @@ namespace SnakeAndLadderProblem
 {
     internal class Player
     {
-       
         public static void Pos1()
         {
+            Console.WriteLine("Welcome To Snake And Ladder Problem");
             int position = 0, player1;
             Console.WriteLine("Single player at start position", +position);
-            Console.WriteLine("Welcome To Snake And Ladder Problem");
-           
-            
         }
         public static void RollDice()
         {
             int position = 0;
+            while (position < 100)
+            {               
             Random random = new Random();
             int dice = random.Next(6);
             dice = dice + 1;
-            Console.WriteLine("The Player Rolls The Dice & Get Number:" + dice);
-            int option = random.Next(0, 3);
-            switch (option)
-            {
-                case 1:
-                    position+= dice;
-                    Console.WriteLine("Ladder");
-                    break;
-                case 2:
-                    position-= dice;
-                    Console.WriteLine("Snake");
-                    break;
-                default:
-                    Console.WriteLine("No Play");
-                    break;
+            Console.WriteLine("The Player Rolls The Dice & Get Number:" + dice);          
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case 1:
+                        position += dice;
+                        Console.WriteLine("Ladder");
+                        if (position > 100)
+                        {
+                            position -= dice;
+                        }
+                        break;
+                    case 2:
+                        position -= dice;
+                        Console.WriteLine("Snake");                      
+                        if (position < 0)
+                        {
+                            position = 0;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("No Play");
+                        break;
+                }
+                Console.WriteLine("Current Position of player is " + position);
             }
-
         }
     }
 }
